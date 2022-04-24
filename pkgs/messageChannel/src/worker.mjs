af044efddbb0ff8ae8e333cfcba472d7bf6e5078
espoal <alberto.websci@gmail.com>
2022-04-24 17:02:55 +0200

    feat: messageChannel works

--- pkgs/messageChannel/src/worker.mjs
+++ pkgs/messageChannel/src/worker.mjs
@@ -1,14 +1,18 @@
 import { parentPort, receiveMessageOnPort } from 'node:worker_threads'
 import { setTimeout } from 'node:timers/promises'
 
-
+/*
 const awaitPort = () =>
   new Promise(res => {
     parentPort.on('message', res)
   })
 const port = await awaitPort()
+*/
+
+const {message} = receiveMessageOnPort(parentPort)
 
 
+const port = message
 let iter = 0
 
 
@@ -16,7 +20,7 @@ let iter = 0
 while ( iter <10) {
 
   port.postMessage(iter)
-  const message = receiveMessageOnPort(port)
+  const {message} = receiveMessageOnPort(port) ?? {message: {}}
 
   console.log({ iter, message})
   iter++
